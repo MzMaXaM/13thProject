25585371c9adf6ff526669959d9b68a86178c72f
MzMaXaM <55605842+MzMaXaM@users.noreply.github.com>
2022-02-18 08:03:14 +0000

    we can update posts

--- routes/blog.js
+++ routes/blog.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
 
 router.get('/posts', async (req, res) => {
   const posts = await db.getDb().collection('posts').find().toArray()
-  res.render('posts-list', {posts: posts})
+  res.render('posts-list', { posts: posts })
 })
 
 router.get('/new-post', async (req, res) => {
@@ -23,7 +23,7 @@ router.post('/new-post', async (req, res) => {
   const Obj_id = new ObjectId(authorId)
   const author = await db.getDb().collection('authors').findOne({ _id: Obj_id })
 
-  try{ 
+  try {
     const newPost = {
       title: req.body.title,
       summary: req.body.summary,
@@ -32,33 +32,69 @@ router.post('/new-post', async (req, res) => {
       author: {
         id: authorId,
         name: author.name,
-        email:author.email
+        email: author.email
       }
     }
     const result = await db.getDb().collection('posts').insertOne(newPost)
-  }catch(err){
+  } catch (err) {
     console.log(err)
-  }finally{
-  res.redirect('/posts')
+  } finally {
+    res.redirect('/posts')
   }
 })
 
-router.get('/detailPost/:id', async(req, res)=>{
+router.get('/detailPost/:id', async (req, res) => {
   let post
-  try{
+  try {
     const postId = (req.params.id).trim()
     const post_id = new ObjectId(postId)
     post = await db.getDb().collection('posts').findOne({ _id: post_id })
-  }catch{
+  } catch {
 
   }
 
-  if(!post){
+  if (!post) {
     return res.status(404).render('404')
   }
-  res.render('post-detail',{
-    post:post
+
+  res.render('post-detail', {
+    post: post
   })
+})
+
+router.get('/updatePost/:id', async (req, res) => {
+  let post
+  try {
+    const postId = (req.params.id).trim()
+    const post_id = new ObjectId(postId)
+    post = await db.getDb().collection('posts').findOne({ _id: post_id })
+  } catch { }
+
+  if (!post) {
+    return res.status(404).render('404')
+  }
+
+  res.render('update-post', {
+    post: post
+  })
+})
+
+router.post('/updatePost/:id', async (req, res) => {
+  const updatedPost = {
+    $set: {
+      title: req.body.title,
+      summary: req.body.summary,
+      body: req.body.content
+    }
+  }
+  let post
+  try {
+    const postId = (req.params.id).trim()
+    const post_id = new ObjectId(postId)
+    post = await db.getDb().collection('posts').updateOne({ _id: post_id }, updatedPost, null)
+  } catch (err) { console.log(err) }
+
+  res.redirect('/posts')
 })
 
 module.exports = router
