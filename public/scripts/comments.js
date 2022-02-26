921c76e8142e297078fc53302676acc70b4d70ff
MzMaXaM <55605842+MzMaXaM@users.noreply.github.com>
2022-02-26 22:04:38 +0000

    we can show comments

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,19 +1,38 @@
 const loadCommentsBtn = document.getElementById('loadCommentsBtn'),
- formSubmit = document.querySelector('#commentsForm form button'),
- titleInput = document.getElementById('title'),
- textInput = document.getElementById('text'),
- commentsSection = document.getElementById('commentsSection')
+  formSubmit = document.querySelector('#commentsForm form button'),
+  titleInput = document.getElementById('title'),
+  textInput = document.getElementById('text'),
+  commentsSection = document.getElementById('commentsSection')
 
 loadCommentsBtn.addEventListener('click', fetchComments)
 formSubmit.addEventListener('click', saveComment)
 
 async function fetchComments() {
-  const postId = loadCommentsBtn.dataset.postid
+  let postId = loadCommentsBtn.dataset.postid
+  console.log(`%c${postId}`, 'color: #7F95D1')
 
   const res = await fetch(`/posts/${postId}/comments`)
   const data = await res.json()
 
+  commentsSection.innerHTML = ''
+  commentsSection.appendChild(writeComments(data))
+}
 
+function writeComments(arr) {
+  let res = document.createElement('ul')
+
+  arr.forEach(comm => {
+    const comment = document.createElement('li')
+    comment.innerHTML = `
+      <div class="comment-item">
+        <h2>${comm.title}</h2>
+        <p>${comm.text}</p>
+      </div>
+    `
+    res.appendChild(comment)
+  })
+
+  return res
 }
 
 async function saveComment(event) {
@@ -35,4 +54,5 @@ async function saveComment(event) {
     }
   })
 
+  fetchComments()
 }
